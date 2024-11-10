b72c2f400b8c39bda688eade591188fbd2a0b4e3
kaiyuran <73236245+kaiyuran@users.noreply.github.com>
2024-11-10 01:41:44 -0500

    confetti! sidebars

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-function changeText() {
-    var catText = document.getElementById("userInput").value.split("\n")
-    // console.log(catText);
-    return catText;
+const jsConfetti = new JSConfetti()
+console.log(jsConfetti)
+
+function changeTopText() {
+    var topText = document.getElementById("topText").value.split("\n")
+    return topText;
+}
+
+function changeBotText() {
+    var botText = document.getElementById("botText").value.split("\n")
+    return botText;
 }
 
 function getImage() {
     let imageNum = Math.floor(Math.random() * 500);
     const image = new Image();
     image.src = `CAT\\CAT_${imageNum}.jpg`;
+    // image.src = "test500.png"
     // document.getElementById("test").src = image.src;
     return image;
 }
 
 function generateMeme() {
-    text = changeText();
+    topText = changeTopText();
+    botText = changeBotText();
     if (document.getElementById("imgURL").value == "") {
         image = getImage();
     }
@@ -27,23 +36,41 @@ function generateMeme() {
 
     var frame = document.getElementById('ImageCanvas');
     var canvas = frame.getContext('2d');
-    // console.log(image);
-    // console.log(canvas);
     
-    let lineMargin = 80;
     image.onload = function() { //buffer to let image load. This took too long to figure out...
         // console.log(this.width + 'x' + this.height);
+
         frame.width = this.width;
         frame.height = this.height;
         canvas.drawImage(this, 0,0);
-        canvas.font = "70px Impact";
+        let fontSize = (40/500)*frame.height;
+
+
+        canvas.font = `${fontSize}px Impact`;
         canvas.fillStyle = "white";
         canvas.strokeStyle = "black";
-        for (let i = 0; i < text.length; i++) {
-            canvas.fillText(text[i],10,lineMargin * (i+1), frame.width-20);
+        //top text
+        for (let i = 0; i < topText.length; i++) {
+            canvas.fillText(topText[i],10,(fontSize * i)+fontSize, frame.width-20);
+        }
+        
+        //bottom text
+        botText.reverse(); //need to reverse to properly display text in order
+        for (let i = 0; i < botText.length; i++) {
+            canvas.fillText(botText[i],10, frame.height - (fontSize * i), frame.width-20);
         }
     }
-    // console.log(canvas);
+    console.log(jsConfetti)
+    jsConfetti.addConfetti({
+        emojis: ['🐈', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '😾', '🐱', '🐈‍⬛']
+    });
 }
 
-
+function downloadMeme() {
+    var canvas = document.getElementById('ImageCanvas');
+    var image = canvas.toDataURL("image/jpg");
+    var link = document.createElement('a');
+    link.href = image;
+    link.download = "meme.jpg";
+    link.click().then(() => console.log("conf"));
+}

9ca32f38988d89e41d61a948ddf028b70cb224d4
kaiyuran <73236245+kaiyuran@users.noreply.github.com>
2024-11-11 22:20:28 -0500

    page 2

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-External things used:
+This site was created to make cat memes using HTML, JS and CSS. It can use a randomly selected image from a database or a user uploaded image. The user can add text to the image and then download the image. Confetti and a cat sound will play when the image is downloaded. This was my first time using HTML canvases and I learned a lot about how to manipulate images in JS.    
+
+If the site is running slow, just keep the developer console open. I'm not sure why but its much faster...
+
+External libraries/resources used:
     https://www.npmjs.com/package/js-confetti
-    https://pixabay.com/sound-effects/search/happy%20cat/ # Sound: Cat
+    https://pixabay.com/sound-effects/search/happy%20cat/ Sound: Cat
+    https://www.pexels.com/ cat videos
---
+++ script1.js
@@ -0,0 +1,84 @@
+
+const jsConfetti = new JSConfetti()
+console.log(jsConfetti)
+
+var catSound = new Audio('resources\\catSound.wav');
+
+function changeTopText() {
+    var topText = document.getElementById("topText").value.split("\n")
+    return topText;
+}
+
+function changeBotText() {
+    var botText = document.getElementById("botText").value.split("\n")
+    return botText;
+}
+
+function getImage() {
+    let imageNum = Math.floor(Math.random() * 500);
+    const image = new Image();
+    image.src = `CAT\\CAT_${imageNum}.jpg`;
+    // image.src = "test500.png"
+    // document.getElementById("test").src = image.src;
+    return image;
+}
+
+function generateMeme() {
+    topText = changeTopText();
+    botText = changeBotText();
+    if (document.getElementById("imgURL").value == "") {
+        image = getImage();
+    }
+    else {
+        image = new Image();
+        // img.crossOrigin = "anonymous";
+        image.src = document.getElementById("imgURL").value;
+        image.onerror = function() {
+            alert("Invalid URL");
+        }
+    };
+
+    var frame = document.getElementById('ImageCanvas');
+    var canvas = frame.getContext('2d');
+    
+    image.onload = function() { //buffer to let image load. This took too long to figure out...
+        // console.log(this.width + 'x' + this.height);
+
+        frame.width = this.width;
+        frame.height = this.height;
+        canvas.drawImage(this, 0,0);
+        let fontSize = (40/500)*frame.height;
+
+
+        canvas.font = `${fontSize}px Impact`;
+        canvas.fillStyle = document.getElementById("textColour").value;
+        canvas.strokeStyle = "black";
+        //top text
+        for (let i = 0; i < topText.length; i++) {
+            canvas.fillText(topText[i],10,(fontSize * i)+fontSize, frame.width-20);
+        }
+        
+        //bottom text
+        botText.reverse(); //need to reverse to properly display text in order
+        for (let i = 0; i < botText.length; i++) {
+            canvas.fillText(botText[i],10, frame.height - (fontSize * i), frame.width-20);
+        }
+    }
+
+}
+
+function downloadMeme() {
+    // jsConfetti.addConfetti({
+    //     emojis: ['🐈', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '😾', '🐱', '🐈‍⬛']
+    // });
+    // catSound.play();
+
+    var canvas = document.getElementById('ImageCanvas');
+    var image = canvas.toDataURL("image/jpeg");
+    var link = document.createElement('a');
+    link.href = image;
+    link.download = "meme.jpeg";
+    // link.onload = function() {
+    link.click();
+    // }
+}
